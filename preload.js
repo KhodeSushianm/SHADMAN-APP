d0a4c4c7f1d0d1737464b3bbdc4dc82568c42517
KhodeSushianm <sushiankhoshkhani@atomicmail.io>
2026-09-16 23:21:45 +0330

    refactor: expose stable database IPC bridge

--- preload.js
+++ preload.js
@@ -1,7 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
-  loadDatabase: () => ipcRenderer.invoke('db:get'),
-  saveDatabase: (base64) => ipcRenderer.invoke('db:save', base64),
-  chooseFolder: () => ipcRenderer.invoke('db:choose-folder'),
+  ready: () => ipcRenderer.invoke('app:ready'),
+  query: (sql, params = []) => ipcRenderer.invoke('db:query', sql, params),
+  run: (sql, params = []) => ipcRenderer.invoke('db:run', sql, params),
+  transaction: statements => ipcRenderer.invoke('db:transaction', statements),
   getStorageInfo: () => ipcRenderer.invoke('db:info'),
+  chooseFolder: () => ipcRenderer.invoke('db:choose-folder'),
+  exportDatabase: () => ipcRenderer.invoke('db:export'),
+  importDatabase: () => ipcRenderer.invoke('db:import'),
+  openFolder: () => ipcRenderer.invoke('app:open-folder')
 });
